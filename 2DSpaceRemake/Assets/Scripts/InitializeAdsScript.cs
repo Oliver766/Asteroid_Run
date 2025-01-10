83b87060fab00af7cd37a27fba086516c1290b3e
Oliver766 <oliver@lancashire5.me.uk>
2025-01-10 12:30:02 +0000

    added add config

--- 2DSpaceRemake/Assets/Scripts/InitializeAdsScript.cs
+++ 2DSpaceRemake/Assets/Scripts/InitializeAdsScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Adverts : MonoBehaviour
+public class InitializeAdsScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
